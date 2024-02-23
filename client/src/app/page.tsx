cd645a3b29ff81dc2ac3b04d6b94cff2d634e99d
Waldo Steenkamp <waldosteenkamp@MBP-van-Waldo.home>
2024-02-23 12:03:34 +0100

    Minor styling changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import { ChatWindow } from "@/components/ChatWindow";
 export default function Home() {
   return (
     <ChatWindow
-      titleText="Local SLM, Small Language Model, POC"
+      titleText="LOCAL RUNNING LANGUAGE MODEL POC"
       placeholder="Ask me anything related to the uploaded content..."
     ></ChatWindow>
   );
--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -93,7 +93,7 @@ export function ChatWindow(props: {
             placeholder={placeholder ?? "Input text here"}
             onChange={(e) => setInput(e.target.value)}
           />
-          <button type="submit" className="shrink-0 px-8 py-4 bg-sky-600 rounded w-28">
+          <button type="submit" className="shrink-0 px-8 py-4 bg-sky-500 rounded w-28">
             <div role="status" className={`${isLoading ? "" : "hidden"} flex justify-center`}>
               <svg aria-hidden="true" className="w-6 h-6 text-white animate-spin dark:text-white fill-sky-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -109,7 +109,7 @@ export function ChatWindow(props: {
   );
 
   return (
-    <div className={`flex flex-col items-center p-4 md:p-8 rounded grow overflow-hidden border bg-black`}>
+    <div className={`flex flex-col items-center p-4 md:p-8 rounded grow overflow-hidden border bg-black/70`}>
       <h2 className={`text-2xl`}>{titleText}</h2>
       {chatInterfaceComponent}
       <ToastContainer/>
