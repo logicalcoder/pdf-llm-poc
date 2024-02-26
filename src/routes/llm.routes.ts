1a1743f9fcce77ce6dcfe4c60336ef4222ee17a5
Waldo Steenkamp <waldosteenkamp@MBP-van-Waldo.home>
2024-02-26 16:11:30 +0100

    Refactored project and placed server app in its own directory. Also completed the README files

--- src/routes/llm.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { findResult } from "../controllers/llm.controllers.js";
-
-const router = express.Router();
-
-router.post("/findResult", findResult);
-
-export default router;
